1d0abb3c5c90369d905158b8320cd131d3882818
Kunal19880711 <kunal21634267+github@gmail.com>
2024-12-01 18:07:42 +0530

    feat: adding library to configure apps

---
+++ lib-config-app/lib/main.js
@@ -0,0 +1,21 @@
+import fs from "fs";
+import path from "path";
+import url from "url";
+import yaml from "js-yaml";
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const configFilePath = path.join(
+  __dirname,
+  ...new Array(2).fill(".."),
+  "config.yaml"
+);
+
+const loadAppConfig = (filePath) => {
+  const config = yaml.load(fs.readFileSync(filePath, "utf8"));
+  Object.keys(config).forEach((key) => {
+    process.env[key] = config[key];
+  });
+};
+
+loadAppConfig(configFilePath);
