94fbea8efe95e729809d2e05c82f91c05536b1e3
Kunal19880711 <kunal21634267+github@gmail.com>
2024-12-01 18:08:17 +0530

    feat: adding library for systemwide constants

---
+++ lib-constants-system/lib/main.js
@@ -0,0 +1,16 @@
+const systemConstants = {};
+
+systemConstants.userRoles = {
+  USER: "user",
+  ADMIN: "admin",
+  PARTNER: "partner",
+};
+
+systemConstants.microserviceNames = [
+  {
+    key: "controlpanel",
+    name: "Control Panel",
+  },
+];
+
+export default systemConstants;
