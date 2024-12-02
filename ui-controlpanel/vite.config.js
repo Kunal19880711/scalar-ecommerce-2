58c3d1fc776ffade8a4ff3f9f3f2d3f83d7e00fc
Kunal19880711 <kunal21634267+github@gmail.com>
2024-12-02 10:56:24 +0530

    fix: update proxy

--- ui-controlpanel/vite.config.js
+++ ui-controlpanel/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/bms": {
+      "/e-commerce": {
         target: "http://localhost:8000",
         changeOrigin: true,
         secure: false,
