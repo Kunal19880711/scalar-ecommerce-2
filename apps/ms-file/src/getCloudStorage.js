19146b065bfd8d788bcd736da8491194ca088be7
Kunal19880711 <kunal21634267+github@gmail.com>
2024-12-16 16:56:25 +0530

    fix: loading google storage credentials

--- apps/ms-file/src/getCloudStorage.js
+++ apps/ms-file/src/getCloudStorage.js
@@ -4,6 +4,8 @@ const state = {
   storage: null,
 };
 
+const PRIVATE_KEY = "private_key";
+
 export function getCloudStorage() {
   if (state.storage) {
     return state.storage;
@@ -15,10 +17,22 @@ export function getCloudStorage() {
     projectId: process.env.GOOGLE_PROJECT_ID,
   };
 
-  if (process.env.GOOGLE_STORAGE_CREDENTIALS) {
-    googleStorageConfig.credentials = process.env.GOOGLE_STORAGE_CREDENTIALS;
+  if (!process.env.GOOGLE_STORAGE_CREDENTIALS) {
+    throw new Error("GOOGLE_STORAGE_CREDENTIALS is not set");
   }
+  googleStorageConfig.credentials = JSON.parse(
+    base64Decode(process.env.GOOGLE_STORAGE_CREDENTIALS)
+  );
+  if (!googleStorageConfig?.credentials?.private_key) {
+    throw new Error("GOOGLE_STORAGE_CREDENTIALS is not set");
+  }
+  googleStorageConfig.credentials[PRIVATE_KEY] =
+    googleStorageConfig.credentials[PRIVATE_KEY].replace(/\\n/g, "\n");
 
   const storage = new Storage(googleStorageConfig);
   return (state.storage = storage);
+}
+
+function base64Decode(data) {
+  return Buffer.from(data, "base64").toString("utf-8");
 }
